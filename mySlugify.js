7964c6d6460c6ca7a8e75db14af2a1e0e4d15a67
Osemwota Osagie Anthony <greyhands2@gmail.com>
2021-01-22 22:23:21 +0100

    Update mySlugify.js

--- mySlugify.js
+++ mySlugify.js
@@ -1,6 +1,6 @@
 module.exports = (name) =>{
-    let splitName = this.name.split(' ');
-    let slug='';
+    let[splitName, slug]  = [this.name.split(' '), ''];
+   
    for( var x=0; x<splitName.length; x++){
        if(splitName.indexOf(splitName[x]) < splitName.length-1) slug+=splitName[x]+"-";
 
